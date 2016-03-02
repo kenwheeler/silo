984626c125d7239b7c77dde79247861f768a5342
Zach Silveira <zackify@gmail.com>
2016-03-02 15:00:12 -0500

    cleaned up bind-actions

--- src/bind-actions.js
+++ src/bind-actions.js
@@ -1,14 +1,8 @@
 export default (currentEffects, currentMutations, dispatch, actions) => {
-  if (typeof Proxy === "undefined") {
-    for (const operation in currentEffects) {
-      actions[operation] = (payload: any) => dispatch(operation, payload);
-    }
-    for (const operation in currentMutations) {
-      actions[operation] = (payload: any) => dispatch(operation, payload);
-    }
-  } else {
-    actions = Proxy.create({
-      get: (proxy, name: string) => (payload: any) => dispatch(name, payload)
-    });
+  for (const operation in currentEffects) {
+    actions[operation] = (payload: any) => dispatch(operation, payload);
+  }
+  for (const operation in currentMutations) {
+    actions[operation] = (payload: any) => dispatch(operation, payload);
   }
 };
